55d1fabf3b090aedc2fa70cb528552dc463183e2
bogdannbv <bogdannbv@gmail.com>
2021-06-15 21:43:44 +0300

    added support for all environment variables to docker-entrypoint.sh. added tini as default init.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 set -e
 
-if [ "${1#-}" == "$1" ]; then
-	exec "$@"
+# check if the first argument passed in looks like a flag
+if [ "${1#-}" != "$1" ]; then
+  set -- tunnelto "$@"
 fi
 
-set -- tunnelto "$@"
+## check if the first argument passed in is tunnelto and if it's the only one
+if [ "$#" = 1 ] && [ "$1" = "tunnelto" ]; then
+  if [ -n "${DASHBOARD_PORT}" ]; then
+    set -- "$@" "--dashboard-port=${DASHBOARD_PORT}"
+  fi
 
-if [ -n "${SUBDOMAIN}" ]; then
-  set -- "$@" "--subdomain=${SUBDOMAIN}"
+  if [ -n "${KEY}" ]; then
+    set -- "$@" "--key=${KEY}"
+  fi
+
+  if [ -n "${SCHEME}" ]; then
+    set -- "$@" "--scheme=${SCHEME}"
+  fi
+
+  if [ -n "${HOST}" ]; then
+    set -- "$@" "--host=${HOST}"
+  fi
+
+  if [ -n "${PORT}" ]; then
+    set -- "$@" "--port=${PORT}"
+  fi
+
+  if [ -n "${SUBDOMAIN}" ]; then
+    set -- "$@" "--subdomain=${SUBDOMAIN}"
+  fi
 fi
+
+set -- tini -- "$@"
 
 exec "$@"
